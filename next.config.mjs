1f0b2021b5907bbad962009eefa15b10a102cf5b
Tejas Dherange <tejasdherange0099@gmail.com>
2025-11-10 11:16:18 +0530

    Add headers configuration for Content-Security-Policy in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+   async headers() {
+    return [
+      {
+        source: '/chat',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests"
+          }
+        ]
+      }
+    ]
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
